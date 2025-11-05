f453a8be352b1c5499f3b519a8e233b927ccecbc
Marriel223 <23-17177@g.batstate-u.edu.ph>
2025-11-05 16:38:40 +0800

    CRUD ON BOOK TABLE

--- BACKEND/models/BookModel.js
+++ BACKEND/models/BookModel.js
@@ -1,6 +1,30 @@
+import { stat } from 'fs';
 import pool from './db.js';
 
 export const getBooks = async () =>{
     const [rows] = await pool.query("SELECT * FROM tbl_book")
     return rows;
+}
+
+export const insertBook = async (title, genre, status) =>{
+   const [result] = await pool.query(
+    "INSERT INTO tbl_book (title, genre, status) VALUES (?, ?, ?)",
+    {title, genre, status}
+   );
+    return result.insertId;
+}
+
+export const updateBook = async (title, genre, status, bookId) =>{
+    const [result] = await pool.query(
+     "UPDATE tblbook SET title = ?, genre = ?, status = ? WHERE bookId = ?",
+     {title, genre, status, bookId}
+    );
+        return result.affectedRows;
+}
+
+export const deleteBook = async (bookId) =>{
+    const [result] = await pool.query(
+        "DELETE FROM tbl_book WHERE bookId = ?",
+    )
+    return result.affectedRows;
 }
